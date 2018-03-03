41416e66bd9e721ce735765ac3c208475259138e
Mavey Ma <maveyma@gmail.com>
2018-03-02 16:02:51 -0800

    Set up root wireframes of tabbar

---
+++ Insert Dreams Here/Insert Dreams Here/LoginViewController.swift
@@ -0,0 +1,28 @@
+//
+//  LoginViewController.swift
+//  Insert Dreams Here
+//
+//  Created by Mavey Ma on 3/2/18.
+//  Copyright © 2018 Mavey Ma. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+  @IBOutlet weak var testEnterButton: UIButton!
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+  @IBAction func onButtonClick(_ sender: Any) {
+  }
+  
+}
