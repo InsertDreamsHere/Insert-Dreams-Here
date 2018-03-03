d7d35ba65fb0bbb235055f6baa0436b08fcd7ce6
MiguelFletes <mfletes@csumb.edu>
2018-03-02 16:22:24 -0800

    added profile details to wireframe

---
+++ Insert Dreams Here/Insert Dreams Here/ProfileViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ProfileViewController.swift
+//  Insert Dreams Here
+//
+//  Created by Michael Fletes on 3/2/18.
+//  Copyright © 2018 Mavey Ma. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    @IBOutlet weak var profilePic: UIImageView!
+    @IBOutlet weak var biogrophyLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
