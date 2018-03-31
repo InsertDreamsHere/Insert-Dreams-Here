02cc62b66e760ba0c239dc7355dd12dac16dece3
johnjing <838633350@qq.com>
2018-03-31 10:12:58 -0700

    Changes to the tableview to make it scrollable

--- Insert Dreams Here/Insert Dreams Here/View Controllers/MapViewController.swift
+++ Insert Dreams Here/Insert Dreams Here/View Controllers/MapViewController.swift
@@ -10,11 +10,13 @@ import UIKit
 
 class MapViewController: UIViewController, UITableViewDataSource {
     @IBOutlet weak var mapTableView: UITableView!
+//    var topOffset: CGFloat = 0.0
+//    private let kTopWindowAspectRatio : CGFloat = 3.0/4.0
     
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return 10
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -26,8 +28,28 @@ class MapViewController: UIViewController, UITableViewDataSource {
     override func viewDidLoad() {
         super.viewDidLoad()
         mapTableView.dataSource = self
+        mapTableView.contentInset = UIEdgeInsets(top: 700, left: 0, bottom: 0, right: 0)
+        mapTableView.contentOffset = CGPoint(x:0, y:1)
         // Do any additional setup after loading the view.
+        //mapTableView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+    
+        mapTableView.estimatedRowHeight = 150
+        mapTableView.rowHeight = UITableViewAutomaticDimension
+        
+
     }
+    
+//    override func viewDidLayoutSubviews() {
+//        super.viewDidLayoutSubviews()
+//        // Calculate the top offset and set it if not corectly set
+//        if(topOffset != CGFloat(mapTableView.bounds.width) * kTopWindowAspectRatio) {
+//            topOffset = CGFloat(mapTableView.bounds.width) * kTopWindowAspectRatio
+//            // Setup table view insets and offsets
+//            let topInset = CGFloat(mapTableView.bounds.height)
+//            mapTableView.contentInset = UIEdgeInsetsMake(topInset, 0, 0, 0);
+//            mapTableView.contentOffset = CGPoint(x:0, y:-topOffset)
+//        }
+//    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
