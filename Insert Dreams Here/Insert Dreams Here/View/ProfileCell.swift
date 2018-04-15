c95f69e334b9435cdb80791efaf85c2ceae4c6f2
MiguelFletes <mfletes@csumb.edu>
2018-04-14 19:39:14 -0700

    profile data is able to display, only users dreams are displayed with correct timestamp in descending order

---
+++ Insert Dreams Here/Insert Dreams Here/View/ProfileCell.swift
@@ -0,0 +1,27 @@
+//
+//  ProfileCell.swift
+//  Insert Dreams Here
+//
+//  Created by Michael Fletes on 4/11/18.
+//  Copyright © 2018 Mavey Ma. All rights reserved.
+//
+
+import UIKit
+class ProfileCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var bodyLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+}
