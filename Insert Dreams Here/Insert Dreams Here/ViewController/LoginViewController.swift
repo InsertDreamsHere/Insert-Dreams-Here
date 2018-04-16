6c8e393e6c56e983c42aa452c4bee446ee447a5d
Mavey Ma <maveyma@gmail.com>
2018-04-16 03:22:42 -0700

    dat UI glowup

--- Insert Dreams Here/Insert Dreams Here/ViewController/LoginViewController.swift
+++ Insert Dreams Here/Insert Dreams Here/ViewController/LoginViewController.swift
@@ -16,7 +16,7 @@ class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.hideKeyboard()
     }
     
     override func didReceiveMemoryWarning() {
