25a9dc47e44e895afd53941a58b1f875cdc2d1df
Mavey Ma <maveyma@gmail.com>
2018-04-22 22:20:16 -0700

    Notif code error

--- Insert Dreams Here/Insert Dreams Here/ViewController/ComposeViewController.swift
+++ Insert Dreams Here/Insert Dreams Here/ViewController/ComposeViewController.swift
@@ -39,24 +39,6 @@ class ComposeViewController: UIViewController {
 
 }
 
-
-extension UIViewController
-{
-  func hideKeyboard()
-  {
-    let tap: UITapGestureRecognizer = UITapGestureRecognizer(
-      target: self,
-      action: #selector(UIViewController.dismissKeyboard))
-
-    view.addGestureRecognizer(tap)
-  }
-
-  @objc func dismissKeyboard()
-  {
-    view.endEditing(true)
-  }
-}
-
 extension UIViewController
 {
   func hideKeyboard()
