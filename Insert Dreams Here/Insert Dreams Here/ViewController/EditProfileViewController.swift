641f3f2f09ff7369b962f1be6550e85beb49e7b9
MiguelFletes <mfletes@csumb.edu>
2018-04-30 11:14:15 -0700

    added profile picture code

--- Insert Dreams Here/Insert Dreams Here/ViewController/EditProfileViewController.swift
+++ Insert Dreams Here/Insert Dreams Here/ViewController/EditProfileViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import Parse
 
-class EditProfileViewController: UIViewController {
+class EditProfileViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     
     @IBOutlet weak var username: UILabel!
@@ -23,37 +23,47 @@ class EditProfileViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        //getUserProfile()
         userBio.text = bio
         userPicture = image
         username.text = PFUser.current()!["username"] as? String
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(imageTapped(tapGestureRecognizer:)))
+        userPicture.isUserInteractionEnabled = true
+        userPicture.addGestureRecognizer(tapGestureRecognizer)
     }
-    func getUserProfile(){
+    
+    @objc func imageTapped(tapGestureRecognizer: UITapGestureRecognizer)
+    {
+        let tappedImage = tapGestureRecognizer.view as! UIImageView
         
-        print("current user")
-        print(PFUser.current()!)
+        // Your action
+        let vc = UIImagePickerController()
+        vc.delegate = self
+        vc.allowsEditing = true
         
+        self.present(vc, animated: true, completion: nil)
         
-        let query = PFQuery(className: "Profile")
-        query.whereKey("author", equalTo: PFUser.current()!)
-        query.includeKey("author.username")
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            print("Camera is available 📸")
+            vc.sourceType = .camera
+        } else {
+            print("Camera 🚫 available so we will use photo library instead")
+            vc.sourceType = .photoLibrary
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [String : Any]) {
+        // Get the image captured by the UIImagePickerController
+        let originalImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        let editedImage = info[UIImagePickerControllerEditedImage]
         
-        query.findObjectsInBackground (block: {(objects:[PFObject]?, error: Error?) -> Void in
-            if error == nil {
-                // The find succeeded.
-                print("Successfully retrieved \(objects!.count) profiles.")
-                // Do something with the found objects
-                if let objects = objects {
-                    self.Profiles = objects
-                    
-                    self.username.text = PFUser.current()!["username"] as? String
-                    self.userBio.text = self.Profiles[0]["bio"] as? String
-                }
-            } else {
-                // Log details of the failure
-                print("Error: \(error!)")
-            }
-        })
+        // Do something with the images (based on your use case)
+        
+        //upload.image = editedImage
+        self.userPicture = editedImage as! UIImageView
+        
+        // Dismiss UIImagePickerController to go back to your original view controller
+        dismiss(animated: true, completion: nil)
     }
     
     override func didReceiveMemoryWarning() {
@@ -62,8 +72,24 @@ class EditProfileViewController: UIViewController {
     }
     
     @IBAction func updateEdit(_ sender: Any) {
-        
-        //NotificationCenter.default.post(name: NSNotification.Name("toProfile"), object: nil)
+        var query = PFQuery(className:"profile")
+        print("date joined")
+        //print(PFUser.current()?.createdAt)
+        query.whereKey("author", equalTo: PFUser.current()!)
+        query.findObjectsInBackground (block: {(objects:[PFObject]?, error: Error?) -> Void in
+            if error == nil {
+                // The find succeeded.
+                print("Successfully retrieved \(objects!.count) Profile.")
+                // Do something with the found objects
+                objects![0]["bio"] = self.userBio.text as String
+                objects![0].saveInBackground()
+                _ = self.navigationController?.popViewController(animated: true)
+            } else {
+                // Log details of the failure
+                print("Error: \(error!)")
+            }
+            
+        })
         
     }
     
